fc931d129b002ba0073a43de6dfc9e8992467367
llanaj041 <llanaj041@gmail.com>
2026-05-27 12:57:06 +0200

    iotibatto

---
+++ app/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDfbvKGH-hDVv9GJVqhOTyKF7GLbJS85js",
+  authDomain: "iotibatto-f2e36.firebaseapp.com",
+  projectId: "iotibatto-f2e36",
+  storageBucket: "iotibatto-f2e36.firebasestorage.app",
+  messagingSenderId: "422980869474",
+  appId: "1:422980869474:web:bf3e9ca0c6f227f5a2a03b"
+};
+
+export const app = initializeApp(firebaseConfig);
